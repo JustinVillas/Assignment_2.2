1b344c648efbaec4a32d95997ab869a86373c44f
JustinVillas <villasjustinlaw@gmail.com>
2021-10-24 02:05:05 +0800

    Initial commit

---
+++ ApplesAndOranges.py
@@ -0,0 +1,4 @@
+number_of_apples = int(input("How many apple/s would you like to buy? "))
+number_of_oranges = int(input("How many orange/s would you like to buy? "))
+total_amount = int((number_of_apples * 20) + (number_of_oranges * 25))
+print(f'The total amount is {total_amount}.')
